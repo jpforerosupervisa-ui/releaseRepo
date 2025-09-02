2f2ca83091d7b3f61fa303fdaa5a143f5376b0ee
Juan Pablo <jpforero.supervisa@gmail.com>
2025-09-02 10:57:44 -0500

    Lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="releaseRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Juan",                         # Tu nombre
+    author_email="jpforero.supervisa@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/jpforerosupervisa-ui/releaseRepo",     # URL del proyecto
+)
